502d8f9f35df59dccbd57ff0ec47f1247e193cd0
gm758 <griffinmichl@gmail.com>
2016-01-13 14:46:31 -0800

    implement queue with functional paradigm

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var front = 0;
+  var back = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[front] = value;
+    front++;
   };
 
   someInstance.dequeue = function() {
+    if (front !== back){
+      var retval = storage[back];
+      delete storage[back];
+      back++;
+      return retval;    
+    }
   };
 
   someInstance.size = function() {
+    return front - back;
   };
 
   return someInstance;
