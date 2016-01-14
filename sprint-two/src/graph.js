4751b3882f165a35813e5d3494c2436a072a6984
gm758 <griffinmichl@gmail.com>
2016-01-13 21:03:52 -0800

    implement graph passing tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,7 +4,6 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
-  this.numberOfNodes = 0;
 };
 
 // ------------------------
@@ -16,6 +15,7 @@ Graph.prototype.addNode = function(node) {
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.nodes;
 };
 
 // ------------------------
@@ -32,21 +32,29 @@ Graph.prototype.removeNode = function(node) {
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return toNode in this.nodes[fromNode];
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode][toNode] = true;
+  this.nodes[toNode][fromNode] = true;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes){
+    cb(key);
+  }
 };
 
 /*
