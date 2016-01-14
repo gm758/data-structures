d2952fb69ece9ab6549b9d678487ba909e00d73a
gm758 <griffinmichl@gmail.com>
2016-01-14 11:02:59 -0800

    implemented bsTree, modified test to use new

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
+}; 
+
+BinarySearchTree.prototype.insert = function(value){
+  //dry this code
+  if(value < this.value){
+    if(this.left===undefined){
+      this.left = new BinarySearchTree(value);
+    }else{
+      this.left.insert(value);
+    }
+  } else{
+    if(this.right===undefined){
+      this.right = new BinarySearchTree(value);
+    }else{
+      this.right.insert(value);
+    }
+  }
 };
 
+BinarySearchTree.prototype.contains = function(value){
+  if (value === this.value){
+    return true;
+  } else if (value < this.value) {
+    if(this.left===undefined) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else {
+    if(this.right===undefined) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+};
 
+BinarySearchTree.prototype.depthFirstLog = function(func){
+  func(this.value);
+
+  if(this.left!==undefined){
+    this.left.depthFirstLog(func);
+  }
+
+  if(this.right!==undefined){
+    this.right.depthFirstLog(func);
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
