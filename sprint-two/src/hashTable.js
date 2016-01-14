8e079acbd2cd85c777101416a31128abc053e32a
gm758 <griffinmichl@gmail.com>
2016-01-14 09:52:07 -0800

    add Tuple and Bucket class to hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -17,6 +17,40 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
+var Tuple = function(key, value){
+  this.key = key;
+  this.value = value;
+};
+
+var Bucket = function(){
+	var tuples = [];
+
+	this.addTuple = function(key, value){
+    var existingIndex = this.find(key);
+
+    if(existingIndex>=0){
+      tuples[existingIndex].value = value;
+    } else{
+      tuples.push(new Tuple(key,value));
+    }
+	};
+
+	this.removeTuple = function(key){
+    var foundIndex = this.find(key);
+    if(foundIndex>=0){
+      tuples = tuples.slice(0,foundIndex) + tuples.slice(foundIndex+1);
+    }
+	};
+
+	this.find = function(key){
+    _.each(tuples, function(tuple, index){
+      if(tuple.key === key){
+        return index;
+      }
+    });	
+    return -1;
+	};
+};
 
 
 /*
