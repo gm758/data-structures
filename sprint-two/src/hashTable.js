fe53a2473a21e05722ccb9c5b6c2cd46d9041649
gm758 <griffinmichl@gmail.com>
2016-01-14 10:29:39 -0800

    implement remaining hash table functionality

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,22 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index) === undefined){
+    this._storage.set(index, new Bucket());
+  }
+  this._storage.get(index).addTuple(k,v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  return bucket.getTuple(k) === undefined ? undefined : bucket.getTuple(k).value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  bucket.removeTuple(k);
 };
 
 var Tuple = function(key, value){
@@ -23,35 +31,44 @@ var Tuple = function(key, value){
 };
 
 var Bucket = function(){
-	var tuples = [];
+	this.tuples = [];
 
-	this.addTuple = function(key, value){
-    var existingIndex = this.find(key);
-
-    if(existingIndex>=0){
-      tuples[existingIndex].value = value;
-    } else{
-      tuples.push(new Tuple(key,value));
-    }
-	};
-
-	this.removeTuple = function(key){
-    var foundIndex = this.find(key);
-    if(foundIndex>=0){
-      tuples = tuples.slice(0,foundIndex) + tuples.slice(foundIndex+1);
-    }
-	};
-
-	this.find = function(key){
-    _.each(tuples, function(tuple, index){
-      if(tuple.key === key){
-        return index;
-      }
-    });	
-    return -1;
-	};
+  
 };
 
+Bucket.prototype.addTuple = function(key, value){
+  var existingIndex = this.find(key);
+
+  if(existingIndex>=0){
+    this.tuples[existingIndex].value = value;
+  } else{
+    this.tuples.push(new Tuple(key,value));
+  }
+};
+
+Bucket.prototype.removeTuple = function(key){
+  var foundIndex = this.find(key);
+  if(foundIndex>=0){
+    this.tuples = this.tuples.slice(0,foundIndex) + this.tuples.slice(foundIndex+1);
+  }
+};
+
+Bucket.prototype.find = function(key){
+  for(var i = 0; i < this.tuples.length; i++){
+    if (this.tuples[i].key === key){
+      return i;
+    }
+  }
+  return -1;
+};
+
+Bucket.prototype.getTuple = function(key){
+  var foundIndex = this.find(key);
+  console.log(this.tuples);
+  console.log(key);
+  console.log(foundIndex);
+  return foundIndex===-1 ? undefined : this.tuples[foundIndex];
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
