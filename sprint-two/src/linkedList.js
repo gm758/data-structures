cb60360eef9222df436165d92169e2ab32c2789e
gm758 <griffinmichl@gmail.com>
2016-01-13 16:25:15 -0800

    implement a linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,51 @@
 var LinkedList = function() {
+  var sizeList = 0;
   var list = {};
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    //Create new node
+    var node = new Node(value);
+
+    //Initialize head if we don't have one
+    list.head = list.head || node;
+
+    if(list.tail!==null){
+      list.tail.next = node;
+    }
+    
+    //set tail to new to new node
+    list.tail = node;
+
+    sizeList++;
+
   };
 
   list.removeHead = function() {
+    var nextHead = list.head.next;
+    var initialHead = list.head;
+    list.head.next = null;
+    list.head = nextHead;
+    sizeList--;
+    return initialHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if(currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
+
+  list.size = function() {
+    return sizeList;
+  }
 
   return list;
 };
