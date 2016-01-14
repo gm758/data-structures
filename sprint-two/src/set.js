d6419a6e45eacf2523781a30c02e1806c2002858
gm758 <griffinmichl@gmail.com>
2016-01-13 21:09:14 -0800

    implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return item in this._storage;
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
