87181f10118dd6e0b179bf0c4c2da0341405efeb
Vince Cipriani <vince.cipriani@gmail.com>
2016-01-16 11:54:08 -0800

    implement t9 autocomplete

--- advanced/src/trie.js
+++ advanced/src/trie.js
@@ -9,6 +9,7 @@ var Trie = function(value){
 
 Trie.prototype.insertWord = function(word){
   var node = this;
+  word = word + '"';
 
   for(var i=0; i<word.length; i++){
     if(!(word[i] in node.children)){
@@ -16,6 +17,7 @@ Trie.prototype.insertWord = function(word){
     } 
     node = node.children[word[i]];
   }
+
 };
 
 Trie.prototype.getWord = function(){
@@ -24,7 +26,7 @@ Trie.prototype.getWord = function(){
 
 
 var Phone = function(){
-  var t9 = {
+  this._t9 = {
     2: 'abc',
     3: 'def',
     4: 'ghi',
@@ -35,57 +37,41 @@ var Phone = function(){
     9: 'wxyz'
   };
 
-  var lookupTree = new Trie();
+  this._lookupTree = new Trie();
   for (var i = 0; i < words.length; i++){
-    lookupTree.insert(words[i]);
+    this._lookupTree.insertWord(words[i]);
   }
 
   this._keys = [];
 };
 
 Phone.prototype.press = function(number){
-  this._keys.push(number);
-}
+  this._keys.push(this._t9[number]);
+};
 
 
+Phone.prototype.getPotentialWords = function() {
 
+  var wordList = [];
 
+  var recursive = function (keyPresses, currentKeyPress,currentWord, node) {
 
+    if (currentKeyPress === keyPresses.length) { 
+        if ('"' in node.children) {
+          wordList.push(currentWord);
+        }
+    } else {
+      for (var i=0; i<keyPresses[currentKeyPress].length; i++) {
+        var potentialLetter = keyPresses[currentKeyPress][i];
 
+        if (potentialLetter in node.children) {
+          recursive(keyPresses, currentKeyPress + 1, currentWord + potentialLetter, node.children[potentialLetter]);
+        }
+      }
+    }
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  recursive(this._keys, 0, '', this._lookupTree);
+  return wordList;
+};
 
